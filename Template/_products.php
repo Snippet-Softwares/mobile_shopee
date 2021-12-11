47ca558df4e61b73a07685a301a9998c497ad6fb
snippet-softwares <snippetsoftwaresolutions@gmail.com>
2021-12-11 11:28:13 +0300

    Updated all files to PHP form structure

---
+++ Template/_products.php
@@ -0,0 +1,130 @@
+<!-- Product -->
+<sectio id="product" class="py-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="./assets/products/1.png" alt="Product 1" class="img-fluid">
+                <div class="form-row pt-4 font-size-16 font-baloo">
+                    <div class="col">
+                        <button class="btn btn-danger form-control">Proceed to buy</button>
+                    </div>
+                    <div class="col">
+                        <button class="btn btn-warning form-control">Add to cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 py-5">
+                <h5 class="font-baloo font-size-20">Samsung Galaxy 10</h5>
+                <small>By Samsung</small>
+                <div class="d-flex">
+                    <div class="rating text-warning font-size-12">
+                        <span><i class="fas fa-star"></i></span>
+                        <span><i class="fas fa-star"></i></span>
+                        <span><i class="fas fa-star"></i></span>
+                        <span><i class="fas fa-star"></i></span>
+                        <span><i class="far fa-star"></i></span>
+                    </div>
+                    <a href="#" class="px-2 font-raleway font-size-14">20.534 ratings | 1000+ answered questions</a>
+                </div>
+                <hr class="m-0">
+                <!-- product price -->
+                <table class="my-3">
+                    <tr class="font-raleway font-size-14">
+                        <td>M.R.P</td>
+                        <td>&nbsp;&nbsp;<strike>$162.00</strike></td>
+                    </tr>
+                    <tr class="font-raleway font-size-14">
+                        <td>Deal Price</td>
+                        <td class="font-size-20 text-danger">&nbsp;<span>$152.00</span><small class="text-dark font-size-12">&nbsp;&nbsp;Inclusive of all taxes</small></td>
+                    </tr>
+                    <tr class="font-raleway">
+                        <td class="font-size-12">You Save</td>
+                        <td class="text-danger font-size-16">&nbsp;<span>$10.00</span></td>
+                    </tr>
+                </table>
+                <!-- End product price -->
+                <!-- Policy -->
+                <div id="policy">
+                    <div class="d-flex">
+                        <div class="return text-center mr-5">
+                            <div class="font-size-20 my-2 color-second">
+                                <span class="fas fa-retweet border p-3 rounded-pill"></span>
+                            </div>
+                            <a href="#" class="font-raleway font-size-12">10 Days<br>Replacement</a>
+                        </div>
+                        <div class="return text-center mr-5">
+                            <div class="font-size-20 my-2 color-second">
+                                <span class="fas fa-truck border p-3 rounded-pill"></span>
+                            </div>
+                            <a href="#" class="font-raleway font-size-12">Daily Tuition<br>Delivered</a>
+                        </div>
+                        <div class="return text-center mr-5">
+                            <div class="font-size-20 my-2 color-second">
+                                <span class="fas fa-check-double border p-3 rounded-pill"></span>
+                            </div>
+                            <a href="#" class="font-raleway font-size-12">1 Year<br>Warranty</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- End policy -->
+                <hr>
+                <!-- Order details -->
+                <div id="order-details" class="font-raleway d-flex flex-column text-dark">
+                    <small>Delivery by: Mar 29 - Apr 1</small>
+                    <small>Sold by: <a href="#">Daily Electronics</a> (4.5 out of 5 | 18,198 ratings)</small>
+                    <small><i class="fas fa-map-marker-alt color-primary"></i>&nbsp;&nbsp;Deliver to Customer - 420401 </small>
+                </div>
+                <!-- End order details -->
+                <div class="row">
+                    <div class="col-6">
+                        <!-- colors -->
+                        <div class="color my-3">
+                            <div class="d-flex justify-content-between">
+                                <h6 class="font-baloo">Color: </h6>
+                                <div class="p-2 color-yellow-bg rounded-circle"><button class="btn font-size-14"></button></div>
+                                <div class="p-2 color-primary-bg rounded-circle"><button class="btn font-size-14"></button></div>
+                                <div class="p-2 color-second-bg rounded-circle"><button class="btn font-size-14"></button></div>
+                            </div>
+                        </div>
+                        <!-- End colors -->
+                    </div>
+                    <div class="col-6">
+                        <!-- product Qty section -->
+                        <div class="qty d-flex">
+                            <h6 class="font-baloo">Qty:</h6>
+                            <div class="px-4 d-flex font-raleway">
+                                <button class="qty-up border bg-light" data-id="pro1"><i class="fas fa-angle-up"></i></button>
+                                <input type="text" data-id="pro1" class="qty_input border px-2 w-50 bg-light text-center" disabled value="1" placeholder="1">
+                                <button data-id="pro1" class="qty-down border bg-light"><i class="fas fa-angle-down"></i></button>
+                            </div>
+                        </div>
+                        <!-- End product Qty section -->
+                    </div>
+                </div>
+                <!-- Size -->
+                <div class="size my-3">
+                    <h6 class="font-baloo">Size:</h6>
+                    <div class="d-flex justify-content-between w-50">
+                        <div class="font-rubik border p-2">
+                            <button class="btn p-0 font-size-14">4GB RAM</button>
+                        </div>
+                        <div class="font-rubik border p-2">
+                            <button class="btn p-0 font-size-14">6GB RAM</button>
+                        </div>
+                        <div class="font-rubik border p-2">
+                            <button class="btn p-0 font-size-14">8GB RAM</button>
+                        </div>
+                    </div>
+                </div>
+                <!-- End size -->
+            </div>
+            <div class="col-12">
+                <h6 class="font-rubik">Product Description</h6>
+                <hr>
+                <p class="font-raleway font-size-14">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur, aspernatur dicta. Reprehenderit nostrum reiciendis ducimus autem natus non voluptate repudiandae ab, nemo tenetur modi quos odit dolores unde esse assumenda!</p>
+                <p class="font-raleway font-size-14">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur, aspernatur dicta. Reprehenderit nostrum reiciendis ducimus autem natus non voluptate repudiandae ab, nemo tenetur modi quos odit dolores unde esse assumenda!</p>
+            </div>
+        </div>
+    </div>
+</sectio>
+<!-- End product -->
